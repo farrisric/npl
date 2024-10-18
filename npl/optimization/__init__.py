cd41cbccf65399872853661fbc8443d390769003
farrisric <farrisric@outlook.com>
2024-10-18 11:34:59 +0200

    Monte Carlo Example

--- npl/optimization/__init__.py
+++ npl/optimization/__init__.py
@@ -1,10 +1,14 @@
 # npl/optimization/__init__.py
 
 from .go_search import GOSearch, MCSearch, GASearch, GuidedSearch
+from .monte_carlo import run_monte_carlo
+from .basin_hopping import run_basin_hopping
+from .genetic_algoritm import run_genetic_algorithm
 
 __all__ = [
     "GOSearch",
     "MCSearch",
     "GASearch",
-    "GuidedSearch"
+    "GuidedSearch", 
+    "run_monte_carlo",
 ]
