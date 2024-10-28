3a0ffd7462a4b96f7356f4b48a8b3c1b5c9c77c7
farrisric <farrisric@outlook.com>
2024-10-28 13:45:06 +0100

    licence name

--- npl/logging_config.py
+++ npl/logging_config.py
@@ -4,7 +4,7 @@ import logging
 
 def setup_logging():
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.DEBUG,
         format='%(name)s - %(levelname)s - %(message)s',
         handlers=[
             logging.FileHandler("npl.log"),
